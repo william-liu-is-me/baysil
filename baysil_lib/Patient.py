d7c55c4727fb2f86ccbab9c56ab668ce105eb2d9
yang's first git file <cqly0103@gmail.com>
2023-02-19 21:45:22 -0500

    update province

--- baysil_lib/Patient.py
+++ baysil_lib/Patient.py
@@ -500,7 +500,7 @@ class Baby(Person):
                 else:
                         province = self.province
 
-                text = str(self.address or '') + ', ' + str(self.city or '') + ', ' + str(self.province or '') + ' ' + str(self.postal_code or '') + ' Canada'
+                text = str(self.address or '') + ', ' + str(self.city or '') + ', ' + str(province or '') + ' ' + str(self.postal_code or '') + ' Canada'
 
         
 
